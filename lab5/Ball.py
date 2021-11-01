49b0e10a5fad1f0d1a1f62e0dda256713eca91dd
Kanaev <kanaev.aiu@phystech.edu>
2021-11-01 16:29:36 +0300

    Some fixes.

--- lab5/Ball.py
+++ lab5/Ball.py
@@ -4,15 +4,19 @@ import pygame.draw
 
 pygame.init()
 
+name = input()
+
 BOARDERS =[0, 0, 400, 400] 
 'game screen coordinates'
 RED = (255, 0, 0)
 BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
 COLORS = [RED, BLUE]
 screencolor = (50, 50, 50)
-numer = 5
+num_standart = 5
+num_epic = 1
 'number of circles'
-finishtime = 500
+finishtime = 700
 globaltime = 0
 FPS = 30
 screen = pygame.display.set_mode((800, 400))
@@ -59,24 +63,14 @@ def timebar():
 class Ball:
 
     def __init__(self):
+        self.status = 0
         self.x = randint(100, 300)
         self.y= randint(100, 300)
-        self.v_x = randint(-5, 5)
-        self.v_y = randint(-5, 5)
-        self.r = randint(15, 30)
+        self.v_x = (self.status + 1) * randint(-5, 5)
+        self.v_y = (self.status + 1) * randint(-5, 5)
+        self.r = randint(20, 30)
         self.color = choice(COLORS)
         self.time = 0
-
-    def move(self):
-
-        '''
-        (function) move: 
-        Function calculates (x, y) coordinates and draws circles.
-        '''
-
-        self.x += self.v_x
-        self.y += self.v_y
-        pygame.draw.circle(screen, self.color,(self.x, self.y), self.r)
 
     def collision(self, BOARDER):
 
@@ -90,13 +84,27 @@ class Ball:
             self.x + self.v_x + self.r >= BOARDER[2] or
             self.x + self.v_x - self.r <= BOARDER[0]
         ):
-            self.v_x = -randint(0, 100) * self.v_x / 100
+            self.v_x = -randint(0, 250) * self.v_x / 100
     
         if (
             self.y + self.v_y - self.r <= BOARDER[1] or
             self.y + self.v_y + self.r >= BOARDER[3]
         ):
-            self.v_y = -randint(0, 100) * self.v_y / 100
+            self.v_y = -randint(0, 250) * self.v_y / 100
+
+
+    def move(self):
+
+        '''
+        (function) move: 
+        Function calculates (x, y) coordinates and draws circles.
+        '''
+
+        self.x += self.v_x
+        self.y += self.v_y
+        pygame.draw.circle(screen, self.color,(self.x, self.y), self.r, width = self.status)
+
+
 
     def collups(self):
 
@@ -108,10 +116,11 @@ class Ball:
         if self.time >= 100:
             self.x = randint(100, 300)
             self.y= randint(100, 300)
-            self.v_x = randint(-5, 5)
-            self.v_y = randint(-5, 5)
-            self.r = randint(15, 30)
-            self.color = choice(COLORS)
+            self.v_x = (self.status + 1) * randint(-5, 5)
+            self.v_y = (self.status + 1) * randint(-5, 5)
+            self.r = abs(self.status - 0.7) * randint(20, 50)
+            if self.status == 0:
+                self.color = choice(COLORS)
             self.time = 0
 
     def hit(self, coord):
@@ -127,12 +136,13 @@ class Ball:
         if (coord[0] - self.x) ** 2 + (coord[1] - self.y) ** 2 <= (self.r) ** 2:
             self.x = randint(100, 300)
             self.y= randint(100, 300)
-            self.v_x = randint(-5, 5)
-            self.v_y = randint(-5, 5)
-            self.r = randint(15, 30)
-            self.color = choice(COLORS)
+            self.v_x = (self.status + 1) * randint(-5, 5)
+            self.v_y = (self.status + 1) * randint(-5, 5)
+            self.r = abs(self.status - 0.7) * randint(20, 50)
+            if self.status == 0:
+                self.color = choice(COLORS)
             self.time = 0
-            score += 1
+            score += self.status * 2 + 1
 
     def miss(self, coord):
 
@@ -145,17 +155,27 @@ class Ball:
         global score, miss_num
 
         if (coord[0] - self.x) ** 2 + (coord[1] - self.y) ** 2 > (self.r) ** 2:
-            if score > 0:
-                miss_num += 1
+            miss_num += 1
 
 
-pool = []
-for k in range(numer):
-    pool.append(Ball())
+pool1 = []
+for k in range(num_standart):
+    pool1.append(Ball())
+
+pool2 = []
+for k in range(num_epic):
+    pool2.append(Ball())
+
+for k in pool2:
+    k.status = 1
+    print(k.status)
+    k.color = YELLOW
+    k.r = 0.3 * randint(20, 30)
 
 
 clock = pygame.time.Clock()
 finished = False
+finished0 = False
 
 schet(580, 235)
 pygame.draw.rect(screen, (255, 255, 255), (420, 20, 360, 60), width = 0)
@@ -163,32 +183,58 @@ pygame.draw.rect(screen, (255, 255, 255), (420, 20, 360, 60), width = 0)
 while not finished:
     clock.tick(FPS)
     for event in pygame.event.get():
+        schet(580, 235)
         if event.type == pygame.QUIT:
             finished = True
-        elif event.type == pygame.MOUSEBUTTONDOWN:
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                finished = True
+        if event.type == pygame.MOUSEBUTTONDOWN:
             if event.button == 1:
-                for k in pool:
+                for k in pool1:
                     k.miss(event.pos)
                     k.hit(event.pos)
-                if miss_num == numer:
+                for k in pool2:
+                    k.miss(event.pos)
+                    k.hit(event.pos)
+                if miss_num == num_standart + num_epic and score > 0:
                     score -= 1
                 miss_num = 0
-                schet(580, 235)
-    for k in pool:
+    for k in pool2:
+        k.collision(BOARDERS)
+        k.time += 0.5
+        k.move()
+        k.collups()
+    for k in pool1:
         k.collision(BOARDERS)
         k.time += 1
         k.move()
         k.collups()
     pygame.display.update()
     
-    globaltime +=1
+    globaltime +=2
     pygame.draw.rect(screen, screencolor, (0, 0, BOARDERS[2], BOARDERS[3]))
     timebar()
 
-    if score == 10:
+    if score >= 10:
         finished = True
         print('YOU WON!')
     if globaltime == finishtime:
         finished = True
         print('YOU LOSE!')
+
+pygame.draw.rect(screen, (0, 0, 0), (0, 0, 800, 400))
+pygame.display.update()
+result = finishtime - globaltime
+print(result)
+
+
+import yaml
+with open("./sps.yaml", "r") as f:
+    loaded = yaml.load(f)
+
+loaded['result'].append({'name': name, 'points': result})
+with open("./sps.yaml", "w") as f:
+    yaml.dump(loaded, f)
 pygame.quit()
+
